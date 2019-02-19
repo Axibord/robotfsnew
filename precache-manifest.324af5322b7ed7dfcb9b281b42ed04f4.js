240daa423891be1e4167ab1bef5190327ea833fa
axibord <ghiles.aitlounis@gmail.com>
2019-02-19 13:21:17 +0100

    added files

---
+++ precache-manifest.324af5322b7ed7dfcb9b281b42ed04f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ff2e6d235ae15c0460e",
+    "url": "/static/css/main.77ab6789.chunk.css"
+  },
+  {
+    "revision": "4ff2e6d235ae15c0460e",
+    "url": "/static/js/main.4ff2e6d2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "55ef30b60dc7cb631986",
+    "url": "/static/css/2.c6fa9443.chunk.css"
+  },
+  {
+    "revision": "55ef30b60dc7cb631986",
+    "url": "/static/js/2.55ef30b6.chunk.js"
+  },
+  {
+    "revision": "7ff12989dd164b054447e92efb798bc4",
+    "url": "/index.html"
+  }
+];
